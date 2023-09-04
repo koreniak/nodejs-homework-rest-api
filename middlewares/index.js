53824cd196e368756e9c032e10ce9305539efcf2
Borys Koreniak <koreniak.borys@gmail.com>
2023-09-04 13:18:44 +0300

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,11 +1,9 @@
 const validateBody = require('./validateBody');
 const isValidId = require('./isValidId');
 const authenticate = require('./authenticate');
-const isValidUserId = require('./isValidUserId')
 
 module.exports = {
   validateBody,
   isValidId,
   authenticate,
-  isValidUserId,
 };
